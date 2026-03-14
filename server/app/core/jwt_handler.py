4b04c4f6bfcd5564a84ab18780499b3a8fe1971a
kiran25krishnan <kirankrishnan910@gmail.com>
2026-03-14 12:40:50 +0530

    added jwt handler

---
+++ server/app/core/jwt_handler.py
@@ -0,0 +1,18 @@
+from jose import jwt
+from datetime import datetime, timedelta
+from app.config import settings
+
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_HOURS = 12
+
+def create_access_token(data: dict):
+
+    to_encode = data.copy()
+
+    expire = datetime.utcnow() + timedelta(hours=ACCESS_TOKEN_EXPIRE_HOURS)
+
+    to_encode.update({"exp": expire})
+
+    encoded_jwt = jwt.encode(to_encode, settings.JWT_SECRET, algorithm=ALGORITHM)
+
+    return encoded_jwt
