9fe8f41077206049fec4da0d42ee2cdcd724d434
deepika4786 <116085560+deepika4786@users.noreply.github.com>
2026-03-14 15:11:46 +0530

    Update main.py

--- server/app/main.py
+++ server/app/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from app.routes import health_routes, auth_routes, patient_routes, user_routes,goal_log_routes,dashboard_routes
 from app.routes import health_tip_routes
 from app.routes import provider_routes
+from app.routes import reminder_routes
+from app.routes import public_routes
 app = FastAPI(title="Healthcare Portal API")
 
 app.include_router(health_routes.router)
@@ -12,3 +14,5 @@ app.include_router(goal_log_routes.router)
 app.include_router(dashboard_routes.router)
 app.include_router(health_tip_routes.router)
 app.include_router(provider_routes.router)
+app.include_router(reminder_routes.router)
+app.include_router(public_routes.router)
