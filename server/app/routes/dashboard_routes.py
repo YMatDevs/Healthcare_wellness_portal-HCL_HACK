3080d089533b4648665e4864d926196b38df5a13
kiran25krishnan <159471304+kiran25krishnan@users.noreply.github.com>
2026-03-14 15:18:30 +0530

    Update dashboard_routes.py

--- server/app/routes/dashboard_routes.py
+++ server/app/routes/dashboard_routes.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter, Depends
-from app.core.auth_dependency import get_current_user
+from app.core.auth_middleware import get_current_user
 from app.services.dashboard_service import get_dashboard
 
-router = APIRouter(prefix="/dashboard", tags=["Dashboard"])
+router = APIRouter(prefix="/dashboard")
 
 
 @router.get("/")
@@ -10,8 +10,4 @@ async def dashboard(current_user = Depends(get_current_user)):
 
     user_id = current_user["user_id"]
 
-    data = await get_dashboard(user_id)
-
-    return {
-        "dashboard": data
-    }
+    return await get_dashboard(user_id)
